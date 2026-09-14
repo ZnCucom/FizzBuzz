9979ebfd3c3c5e2d06c9059ab122b3fa6612090d
Zhe Qian <zheqian@Zhes-MacBook-Pro.local>
2026-09-14 16:23:37 -0400

    helper function added

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -5,35 +5,31 @@ package lab1;
  */
 class FizzBuzz {
 
+    private static void doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+    }
+
     public static void main(String[] args) {
 
         int i = 1;
 
         while (i <= 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+           doFizzBuzz(i);
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-            i += 1;
+           i += 1;
+            };
         }
     }
-}
+
